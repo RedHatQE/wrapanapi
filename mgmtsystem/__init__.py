c28c41af78c390f13f7591a6491f6583a4384985
jteehan <jteehan@localhost.localdomain>
2016-01-07 14:38:56 -0800

    Initial Azure RM Management System

--- mgmtsystem/__init__.py
+++ mgmtsystem/__init__.py
@@ -6,6 +6,7 @@ from ec2 import EC2System  # NOQA
 from openstack import OpenstackSystem  # NOQA
 from rhevm import RHEVMSystem  # NOQA
 from scvmm import SCVMMSystem  # NOQA
+from azure import AzureSystem  # NOQA
 from virtualcenter import VMWareSystem  # NOQA
 from kubernetes import Kubernetes  # NOQA
 from openshift import Openshift  # NOQA
