2bd686b2ba44e1ba3a95b8410999d53922ecf906
Mike Shriver <mshriver@redhat.com>
2017-07-07 09:19:49 -0400

    Update virtualcenter.get_vm_datastore_path
    vm.config.datastoreUrl returns an object, use __getattribute__ instead
    of __getitem__

--- wrapanapi/virtualcenter.py
+++ wrapanapi/virtualcenter.py
@@ -515,9 +515,10 @@ class VMWareSystem(WrapanapiAPIBase):
 
     def get_vm_datastore_path(self, vm_name, vm_config_datastore):
         vm = self._get_vm(vm_name)
-        datastore_url = [str(datastore['url']) for datastore in vm.config.datastoreUrl
-                         if datastore['name'] in vm_config_datastore]
-        return datastore_url[0]
+        datastore_url = [str(datastore.url)
+                         for datastore in vm.config.datastoreUrl
+                         if datastore.name in vm_config_datastore]
+        return datastore_url.pop()
 
     def get_vm_config_files_path(self, vm_name):
         vm = self._get_vm(vm_name)
