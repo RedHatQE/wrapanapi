3bff1be776e0a2fbac191b51f04d9782806aeb7f
Andrey Menezes <andrey.menezes@gmail.com>
2017-11-08 11:27:50 -0300

    Add inventory method resourses for LenovoSystem

--- wrapanapi/lenovo.py
+++ wrapanapi/lenovo.py
@@ -1,29 +1,28 @@
 # coding: utf-8
 """Backend management system classes
-
 Used to communicate with providers without using CFME facilities
 """
-import requests
 import json
-from base import WrapanapiAPIBase
 from urlparse import urlunparse
+
+import requests
 from requests.exceptions import Timeout
+
+from base import WrapanapiAPIBase
 
 
 class LenovoSystem(WrapanapiAPIBase):
     """Client to Lenovo API
-
     Args:
         hostname: The hostname of the system.
         username: The username to connect with.
         password: The password to connect with.
-
     """
     _api = None
 
     _stats_available = {
-        'num_server': lambda self: len(self.list_server()),
-        'num_server_with_host': lambda self: len(self.list_server_with_host()),
+        'num_server': lambda self: len(self.list_servers()),
+        # 'num_server_with_host': lambda self: len(self.list_servers_with_host()),
     }
     POWERED_ON = 8
     POWERED_OFF = 5
@@ -44,7 +43,7 @@ class LenovoSystem(WrapanapiAPIBase):
         # This isn't the best place for this, but this class doesn't know when it is no longer in
         # use, and we need to do some sort of disconnect based on the pyVmomi documentation.
 
-    def __service_instance(self, path):
+    def _service_instance(self, path):
         """An instance of the service"""
         try:
             uri = urlunparse((self.protocol, self.hostname, path, "", "", ""))
@@ -56,19 +55,19 @@ class LenovoSystem(WrapanapiAPIBase):
     @property
     def version(self):
         """The product version"""
-        response = self.__service_instance("/aicc")
+        response = self._service_instance("/aicc")
         return response['appliance']['version']
 
     def list_servers(self):
-        response = self.__service_instance("/cabinet?status=includestandalone")
+        response = self._service_instance("/cabinet?status=includestandalone")
         cabinets = response['cabinetList'][0]
         nodes_list = cabinets['nodeList']
 
-        inventory_list = map(lambda x: x['itemInventory'], nodes_list)
-        inventory_list = filter(lambda x: x['type'] != 'SCU', inventory_list)
+        inventory_list = [node['itemInventory'] for node in nodes_list]
 
-        chassis_nodes_list = cabinets['chassisList']
-        nodes_from_chassis_list = map(lambda x: x['itemInventory']['nodes'], chassis_nodes_list)
+        chassis_list = cabinets['chassisList'][0]
+        nodes_from_chassis_list = [node for node in chassis_list['itemInventory']['nodes']
+                            if node['type'] != 'SCU']
 
         inventory_list.extend(nodes_from_chassis_list)
         return inventory_list
@@ -81,3 +80,141 @@ class LenovoSystem(WrapanapiAPIBase):
                     return node
         except AttributeError:
             return None
+
+    def get_led(self, server_name):
+        try:
+            server = self.get_server(server_name)
+            leds = server['leds']
+            for led in leds:
+                if led['name'] == 'Identify' or led['name'] == 'Identification':
+                    return led
+        except AttributeError:
+            return None
+
+    def get_server_hostname(self, server_name):
+        server = self.get_server(server_name)
+
+        return str(server['hostname'])
+
+    def get_server_ipv4_address(self, server_name, timeout=600):
+        server = self.get_server(server_name)
+
+        return server['ipv4Addresses']
+
+    def get_server_ipv6_address(self, server_name, timeout=600):
+        server = self.get_server(server_name)
+
+        return server['ipv6Addresses']
+
+    def get_server_mac_address(self, server_name, timeout=600):
+        server = self.get_server(server_name)
+
+        return server['macAddress']
+
+    def server_status(self, server_name):
+        server = self.get_server(server_name)
+
+        if server['powerStatus'] == self.POWERED_ON:
+            return "on"
+        elif server['powerStatus'] == self.POWERED_OFF:
+            return "off"
+        elif server['powerStatus'] == self.STANDBY:
+            return "Standby"
+        else:
+            return "Unknown"
+
+    def server_health(self, server_name):
+        server = self.get_server(server_name)
+
+        if str(server['cmmHealthState'].lower()) in self.HEALTH_VALID:
+            return "Valid"
+        elif str(server['cmmHealthState'].lower()) in self.HEALTH_WARNING:
+            return "Warning"
+        elif str(server['cmmHealthState'].lower()) in self.HEALTH_CRITICAL:
+            return "Critical"
+        else:
+            return "Unknow"
+
+    def is_server_running(self, server_name):
+        server = self.get_server(server_name)
+
+        return server['powerStatus'] == self.POWERED_ON
+
+    def is_server_stopped(self, server_name):
+        server = self.get_server(server_name)
+
+        return server['powerStatus'] == self.POWERED_OFF
+
+    def is_server_standby(self, server_name):
+        server = self.get_server(server_name)
+
+        return server['powerStatus'] == self.STANDBY
+
+    def is_server_valid(self, server_name):
+        server = self.get_server(server_name)
+
+        return str(server['cmmHealthState'].lower()) in self.HEALTH_VALID
+
+    def is_server_warning(self, server_name):
+        server = self.get_server(server_name)
+
+        return str(server['cmmHealthState'].lower()) in self.HEALTH_WARNING
+
+    def is_server_critical(self, server_name):
+        server = self.get_server(server_name)
+
+        return str(server['cmmHealthState'].lower()) in self.HEALTH_CRITICAL
+
+    def is_server_led_on(self, server_name):
+        led = self.get_led(server_name)
+
+        return led['state'] == 'On'
+
+    def is_server_led_off(self, server_name):
+        led = self.get_led(server_name)
+
+        return led['state'] == 'Off'
+
+    def is_server_led_blink(self, server_name):
+        led = self.get_led(server_name)
+
+        return led['state'] == 'Blinking'
+
+    def get_server_cores(self, server_name):
+        server = self.get_server(server_name)
+        processors = server['processors']
+        cores = sum([processor['cores'] for processor in processors])
+
+        return cores
+
+    def get_server_memory(self, server_name):
+        server = self.get_server(server_name)
+        memorys = server['memoryModules']
+        total_memory = sum([memory['capacity'] for memory in memorys])
+
+        return total_memory
+
+    def get_server_manufacturer(self, server_name):
+        server = self.get_server(server_name)
+
+        return str(server['manufacturer'])
+
+    def get_server_model(self, server_name):
+        server = self.get_server(server_name)
+
+        return str(server['model'])
+
+    def get_server_machine_type(self, server_name):
+        server = self.get_server(server_name)
+
+        return str(server['machineType'])
+
+    def get_server_serial_number(self, server_name):
+        server = self.get_server(server_name)
+
+        return str(server['serialNumber'])
+
+    def get_server_description(self, server_name):
+        server = self.get_server(server_name)
+
+        return str(server['description'])
